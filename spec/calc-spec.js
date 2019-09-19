fc3d4866c63cd716824c06eebdfe82dd004014c0
Akigawa <sonoskeakigawa@gmail.com>
2019-09-19 12:44:00 -0700

    should be done

--- spec/calc-spec.js
+++ spec/calc-spec.js
@@ -1,27 +1,110 @@
 import { Calc } from "../src/calc";
 
-describe("the calculator", function(){
-it("can take input",function(){
-var earth = new Calc("Earth", 27, 365, 75);
-var venus = new Calc("venus", 27, 224.7, 75);
-var mercury = new Calc("mercury", 27, 87.97, 75);
-var mars = new Calc("mars", 27, (earth.planetyear * 1.88), 75);
-var jupiter = new Calc("Jupiter", 27, (earth.planetyear * 11.86), 75);
-var saturn = new Calc("Saturn", 27, (earth.planetyear*29.46), 75);
-var uranus = new Calc("ha ha butt planet", 27, 84.01, 75);
-var neptune = new Calc("Neptune", 27, (earth.planetyear * 164.79), 75);
-var pluto = new Calc("pluto", 27, (earth.planetyear * 248.59), 75);
+describe("the calculator", function(){  
+    let earth = new Calc("Earth", 27, 365, 75);
+    let venus = new Calc("venus", 27, 224.7, 75);
+    let mercury = new Calc("mercury", 27, 87.97, 75);
+    let mars = new Calc("mars", 27, (earth.planetyear * 1.88), 75);
+    let jupiter = new Calc("Jupiter", 27, (earth.planetyear * 11.86), 75);
+    let saturn = new Calc("Saturn", 27, (earth.planetyear*29.46), 75);
+    let uranus = new Calc("ha ha butt planet", 27, 84.01, 75);
+    let neptune = new Calc("Neptune", 27, (earth.planetyear * 164.79), 75);
+    let pluto = new Calc("pluto", 27, (earth.planetyear * 248.59), 75);
+    let planets = [earth, venus, mercury, mars, jupiter, saturn, uranus, neptune, pluto];
 
+    //earth
 
-console.log(earth.runcalcs());
-console.log(venus.runcalcs());
-console.log(mercury.runcalcs());
-console.log(mars.runcalcs());
-console.log(jupiter.runcalcs());
-console.log(saturn.runcalcs());
-console.log(uranus.runcalcs());
-console.log(neptune.runcalcs());
-console.log(pluto.runcalcs());
-}); 
+    it(`earth calculates age by planet properly`, function(){
+        
+        console.log(earth.runcalcs());
+        expect(earth.agebyplanet).toBe(27);
+    });
+    it(`earth calculates age by planet properly`, function(){
+        
+        console.log(earth.runcalcs());
+        expect(earth.expectancybyplanet).toBe(48);
+    });
 
+    //venus
+
+    it(`venus calculates age by planet properly`, function(){
+        
+        console.log(venus.runcalcs());
+        expect(venus.agebyplanet).toBe(43);
+    });
+    it(`venus calculates age by planet properly`, function(){
+        console.log(venus.runcalcs());
+        expect(venus.expectancybyplanet).toBe(78);
+    });
+
+    //mercury
+    it(`mercury calculates age by planet properly`, function(){
+        
+        console.log(mercury.runcalcs());
+        expect(mercury.agebyplanet).toBe(112);
+    });
+    it(`mercury calculates age by planet properly`, function(){
+        console.log(mercury.runcalcs());
+        expect(mercury.expectancybyplanet).toBe(199);
+    });
+    //mars
+    it(`mars calculates age by planet properly`, function(){
+        
+        console.log(mars.runcalcs());
+        expect(mars.agebyplanet).toBe(14);
+    });
+    it(`mars calculates age by planet properly`, function(){
+        console.log(mars.runcalcs());
+        expect(mars.expectancybyplanet).toBe(25);
+    });
+    //jupiter
+    it(`jupiter calculates age by planet properly`, function(){
+        
+        console.log(jupiter.runcalcs());
+        expect(jupiter.agebyplanet).toBe(2);
+    });
+    it(`jupiter calculates age by planet properly`, function(){
+        console.log(jupiter.runcalcs());
+        expect(jupiter.expectancybyplanet).toBe(4);
+    });
+    //saturn
+    it(`saturn calculates age by planet properly`, function(){
+        
+        console.log(saturn.runcalcs());
+        expect(saturn.agebyplanet).toBe(0);
+    });
+    it(`saturn calculates age by planet properly`, function(){
+        console.log(saturn.runcalcs());
+        expect(saturn.expectancybyplanet).toBe(2);
+    });
+    //the butt planet
+    it(`uranus calculates age by planet properly`, function(){
+        
+        console.log(uranus.runcalcs());
+        expect(uranus.agebyplanet).toBe(117);
+    });
+    it(`uranus calculates age by planet properly`, function(){
+        console.log(uranus.runcalcs());
+        expect(uranus.expectancybyplanet).toBe(208);
+    });
+    //neptune
+    it(`neptune calculates age by planet properly`, function(){
+        
+        console.log(neptune.runcalcs());
+        expect(neptune.agebyplanet).toBe(0);
+    });
+    it(`neptune calculates age by planet properly`, function(){
+        console.log(neptune.runcalcs());
+        expect(neptune.expectancybyplanet).toBe(0);
+    });
+    //pluto
+    it(`pluto calculates age by planet properly`, function(){
+        
+        console.log(pluto.runcalcs());
+        expect(pluto.agebyplanet).toBe(0);
+    });
+    it(`pluto calculates age by planet properly`, function(){
+        console.log(pluto.runcalcs());
+        expect(pluto.expectancybyplanet).toBe(0);
+    });
 });
--- src/calc.js
+++ src/calc.js
@@ -8,23 +8,39 @@ export class Calc{
     }
     
     calculateplanetyear(){
-        this.averagebyplanet = this.average/this.planetyear;   
-        this.agebyplanet = (this.age * 365)/this.planetyear;
+        this.averagebyplanet = (this.average * 365)/this.planetyear;   
+        this.agebyplanet = parseInt((this.age * 365)/this.planetyear);
     }
     calculateExpectancy(){
-        this.expectancy = (this.age - this.average);
-        this.expectancybyplanet = this.agebyplanet - this.averagebyplanet;
+        this.expectancy = (this.average - this.age  );
+        this.expectancybyplanet = parseInt( this.averagebyplanet - this.agebyplanet);
     }
     outPut(){
         this.output = `Your age is ${this.age} and the average lifespan for your generation is ${this.average}.`;
-        this.output += `Your age on ${this.planetname} is ${this.agebyplanet}!`;
-        if(this.expectancy < 0){
+        if(this.agebyplanet == 0){
+            this.output += `You would be less than a year old on ${this.planetname}!`;
+        }
+        else{
+            this.output += `Your age on ${this.planetname} is ${this.agebyplanet}!`;
+        }
+        if(this.expectancy > 0){
             this.output += `You are most likely to live for ${Math.abs(this.expectancy)} more years!`;
         }
-        if(this.expectancy>0){
-            this.output += `You've lived ${parseMath.abs(this.expectancy)} past life expectency!`;
+        if(this.expectancy<0){
+            this.output += `You've lived ${Math.abs(this.expectancybyplanet)} past life expectency!`;
         }
-        this.output += `Your life expectancy on ${this.planetname} is ${this.expectancybyplanet}!`;
+        if(this.expectancybyplanet > 0){
+            this.output += `You are most likely to live for ${Math.abs(this.expectancybyplanet)} more years!`;
+        }
+        if(this.expectancybyplanet<0){
+            this.output += `You've lived ${Math.abs(this.expectancybyplanet)} past life expectency!`;
+        }
+        if(this.expectancy == 0){
+            this.output += "Wow! You likely have less than a year left!";
+        }
+        if(this.expectancybyplanet == 0){
+            this.output += `Wow! You would likely have less than a year left living on ${this.planetname}`;
+        }
 return this.output;        
     }
     runcalcs(){
